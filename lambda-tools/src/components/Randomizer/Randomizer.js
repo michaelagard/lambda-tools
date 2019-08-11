3af415eee0e5347cfdb6f959dc543af946f1feff
Michael Agard <mdagard@gmail.com>
2019-08-10 23:57:50 -0400

    add comments, clean time gen/time gen vars

--- lambda-tools/src/components/Randomizer/Randomizer.js
+++ lambda-tools/src/components/Randomizer/Randomizer.js
@@ -8,58 +8,63 @@ import Button from '../Button';
 function Randomizer() {
   const titleData = {title: "Randomizer", titleDescription: "The Lambda Randomizer is designed to make the team lead's job easier. Paste your list of stuends in the text area below and click the Shuffle button to recieve a randomly sorted list of your students with their automatically assigned time slots. Now you can just copy and paste the time slots into your team's Slack channel. This tool currently only supports military time."}  
   const [nameArray, setNameArray] = useState("")
-  const [time, setTime] = useState("");
-  const [timeIncrement, setTimeIncrement] = useState("");
+  const [timeStr, setTimeStr] = useState("");
+  const [timeIncrementStr, setTimeIncrementStr] = useState("");
   const [shuffledNameArray, setShuffledNameArray] = useState("")
 
   const handleNameChange = (event) => { setNameArray(event.target.value); }
-  const handleTimeChange = (event) => { setTime(event.target.value); }
-  const handleTimeIncrementChange = (event) => { setTimeIncrement(event.target.value); }
+  const handleTimeChange = (event) => { setTimeStr(event.target.value); }
+  const handleTimeIncrementChange = (event) => { setTimeIncrementStr(event.target.value); }
   
+  // saves nameArray & timeStr & timeIncrementStr to localstorage
   const handleSaveData = event => {
     event.preventDefault();
     localStorage.setItem("nameArray", JSON.stringify(nameArray.split("\n").filter(function(name) { return name.trim() !== ''; })));
-    localStorage.setItem("time", JSON.stringify(time));
-    localStorage.setItem("timeIncrement", JSON.stringify(timeIncrement))
+    localStorage.setItem("time", JSON.stringify(timeStr));
+    localStorage.setItem("timeIncrement", JSON.stringify(timeIncrementStr))
   }
-
+  // loads nameArray & timeStr & timeIncrementStr to localstorage
   const handleLoadData = event => {
     event.preventDefault();
     setNameArray(JSON.parse(localStorage.getItem("nameArray")).join("\n"));
-    setTime(JSON.parse(localStorage.getItem("time")));
-    setTimeIncrement(JSON.parse(localStorage.getItem("timeIncrement")));
+    setTimeStr(JSON.parse(localStorage.getItem("time")));
+    setTimeIncrementStr(JSON.parse(localStorage.getItem("timeIncrement")));
   };
 
+  // Clicking the button associated, uses the nameArray
   const handleShuffleNameArray = event => {
     event.preventDefault();
-    let shuffledNameArray = nameArray.split("\n").sort((name1,name2) => 0.5 - Math.random());
-    let output = addTimeSlotsToShuffledArray(generateMilitaryTimeSlots(shuffledNameArray.length - 1), shuffledNameArray);
-    setShuffledNameArray(output);
+
+    let shuffledNameArray = nameArray.split("\n").sort(() => 0.5 - Math.random());
+    let shuffledNameAndTimeArray = addTimeSlotsToShuffledArray(generateMilitaryTimeSlots(shuffledNameArray.length - 1, parseInt(timeStr.replace(":", "")), parseInt(timeIncrementStr)), shuffledNameArray);
+    
+    setShuffledNameArray(shuffledNameAndTimeArray);
   };
 
-  const generateMilitaryTimeSlots = (shuffledNameArrayLength) => {
+  const parseTimeFromString = (timeStr) => {
+    const [minute, hour] = [parseInt(timeStr.substr(3,2).padStart(2, '0')), parseInt(timeStr.substr(0,2).padStart(2, '0'))];
+    return [minute, hour];
+  }
+
+  // Generates time slots from timeStr/timeIncrementStr state 
+  const generateMilitaryTimeSlots = (shuffledNameArrayLength, fullTimeInt, timeIncrementInt) => {
     let timeSlotArray = [];
-    let timeIncrementInt = parseInt(timeIncrement);
-    let minuteStr = time.toString().substr(3,2).padStart(2, '0');
-    let hourStr = time.toString().substr(0,2).padStart(2, '0');
-    let fullTimeStr = hourStr + minuteStr;
-    let minuteInt = parseInt(time.toString().substr(3,2).padStart(2, '0'));
-    let hourInt = parseInt(time.toString().substr(0,2).padStart(2, '0'));
-    let fullTimeInt = parseInt(hourStr + minuteStr);
+    let fullTimeStr = fullTimeInt.toString().padStart(4, '0');
+    // Used to check conditionals below
+    let [minuteInt, hourInt] = parseTimeFromString(fullTimeStr);
     
-    timeSlotArray.push(fullTimeInt.toString().padStart(4, '0'));
-     
+    timeSlotArray.push(fullTimeStr.replace(":", ""));
     for (let i = 0; i < shuffledNameArrayLength; i++) {
+      // Checks if the time is > than 2360 after incrementing
       if ((fullTimeInt + timeIncrementInt) >= 2360) {
-
         hourInt = 0;
-        minuteInt = fullTimeInt % 60;
+        minuteInt = (minuteInt + timeIncrementInt) % 60;
+        // Checks if the minute is >= 60
       } else if (minuteInt + timeIncrementInt >= 60) {
-
         minuteInt = (minuteInt + timeIncrementInt) % 60;
         hourInt += 1;
-      } else if (minuteInt + timeIncrementInt <= 60) {
-
+        // Checks if the minute is <= 60
+      } else if (minuteInt + timeIncrementInt < 60) {
         minuteInt += timeIncrementInt;
       }
 
@@ -67,15 +72,22 @@ function Randomizer() {
       fullTimeStr = hourInt.toString().padStart(2, '0') + minuteInt.toString().padStart(2, '0')
       timeSlotArray.push(fullTimeStr);
     }
-    
     return timeSlotArray;
   }
 
+  // Takes in the generated time slot array and shuffled name array and outputs
+  // the shuffled name array with time slots hyphonated to each name.
+  // EG: input: [ "2105", "2125"] | Array(7) [ "Foo Bar", "Roo Far" ]
+  // output: [ "2105 - Foo Bar", "2125 - Roo Far" ]
+
   const addTimeSlotsToShuffledArray = (timeSlotsArray, shuffledNameArray) => {
     let randomTimeSlotAndNameArray = [];
+
     for (let i = 0; i < shuffledNameArray.length; i++) {
-      randomTimeSlotAndNameArray.push([timeSlotsArray[i], " - ", shuffledNameArray[i]].join(""));
+      const arrayFormat = [timeSlotsArray[i], " - ", shuffledNameArray[i]];
+      randomTimeSlotAndNameArray.push(arrayFormat.join(""));
     }
+    
     return randomTimeSlotAndNameArray;
   }
 
@@ -102,7 +114,7 @@ function Randomizer() {
             <InputForm formName="Starting Time"
               type={"time"}
               formClassName="randomizer-time-form"
-              inputText={time}
+              inputText={timeStr}
               handleInputText={handleTimeChange}
               placeHolderText={"HH:MM"}
               maxLength="8"
@@ -110,7 +122,7 @@ function Randomizer() {
             <InputForm formName="Minute Increment"
               type={"number"}
               formClassName="randomizer-time-form"
-              inputText={timeIncrement}
+              inputText={timeIncrementStr}
               handleInputText={handleTimeIncrementChange}
               // placeHolderText={"HH:MM"}
               // maxLength="8"
