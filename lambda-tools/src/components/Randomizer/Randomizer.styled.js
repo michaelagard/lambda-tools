1feb4f53fb13ad83fa924d9ac2c07c87265c8efa
Michael Agard <mdagard@gmail.com>
2019-08-12 10:00:47 -0400

    rename container and add style to form

--- lambda-tools/src/components/Randomizer/Randomizer.styled.js
+++ lambda-tools/src/components/Randomizer/Randomizer.styled.js
@@ -1,10 +1,16 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
+
+export const Wrapper = styled.div`
   margin: 55px 8px;
 `;
 
 export const RandomizerForm = styled.form`
   display: flex;
   flex-direction: column;
+  justify-self: center;
+  /* width: 300px; */
+  background: white;
+  margin-top: 2rem;
+  padding: 2rem 2rem 3rem;
 `;
