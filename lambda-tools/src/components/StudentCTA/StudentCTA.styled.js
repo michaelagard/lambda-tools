08186204a724457affee3aceb6bb1f2aa8fd6227
Michael Agard <mdagard@gmail.com>
2019-09-01 17:10:40 -0400

    style CTA form

--- lambda-tools/src/components/StudentCTA/StudentCTA.styled.js
+++ lambda-tools/src/components/StudentCTA/StudentCTA.styled.js
@@ -3,3 +3,12 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
   margin: 55px 8px;
 `;
+
+export const CTAForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-self: center;
+  background: white;
+  margin-top: 2rem;
+  padding: 2rem 2rem 3rem;
+`;
