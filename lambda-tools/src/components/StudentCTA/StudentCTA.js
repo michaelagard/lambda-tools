9a6b210a19fecdcabfa7013a987569ad659c577a
Michael Agard <mdagard@gmail.com>
2019-09-08 21:08:21 -0400

    start cleaning up CTA data

--- lambda-tools/src/components/StudentCTA/StudentCTA.js
+++ lambda-tools/src/components/StudentCTA/StudentCTA.js
@@ -5,74 +5,108 @@ import ToolTitle from '../Subcomponents/ToolTitle/ToolTitle';
 import InputForm from '../Subcomponents/Forms/InputForm';
 import TextAreaForm from '../Subcomponents/Forms/TextAreaForm';
 import { Wrapper, CTAForm } from './StudentCTA.styled';
+import { standUpStrData, moduleReviewAnnouncementStrData, moduleReviewStrData, tkPrepStrData } from './Data/StudentCTAData';
+import Button from '../Subcomponents/Button/Button';
+
 const titleData = {title: "Student CTAs", titleDescription: "For the truely lazy TL who just wants to prompt students to join their Zoom."}
 
 function StudentCTA() {
 
-  const [moduleAssignmentStr, setModuleAssignmentStr] = useState("");
-  const [standUpStr, setStandUpStr ] = useState("@channel\nPlease join me for your daily stand up meeting!\n\n1. What did you do today?\n2. Did you make great progress?\n3. Any blockers you had to overcome?\n4. Are you stuck on something currently?\n\nZOOM_LINK");
-  const [moduleReviewAnnouncementStr, setModuleReviewAnnouncementStr] = useState("@channel\nToday I'll be conducting our module reviews for the assignment `MODULE_ASSIGNMENT`. Below are the times I've randomly generated through ");
-  const [moduleReviewStr, setModuleReviewStr] = useState("Are you prepared for your 1:1, STUDENT_NAME?\nZOOM_LINK");
-  const [tkPrepStr, setTKPrepStr] = useState("@channel\nToday we'll be going over TK_TOPIC that the instructor will go over.\nZOOM_LINK");
-  
-  const handleModuleAssignmentStr = (event) => { setModuleAssignmentStr(event.target.value); }
+  const [zoomLinkStr, setZoomLinkStr] = useState("");
+  const [startTimeStr, setStartTimeStr] = useState("");
+  const [TKTopicStr, setTKTopicStr] = useState("");
 
+  const [standUpStr, setStandUpStr ] = useState(standUpStrData);
+  const [moduleReviewAnnouncementStr, setModuleReviewAnnouncementStr] = useState(moduleReviewAnnouncementStrData);
+  const [moduleReviewStr, setModuleReviewStr] = useState(moduleReviewStrData);
+  const [tkPrepStr, setTKPrepStr] = useState(tkPrepStrData);
+
+  const handleZoomLinkStr = (event) => { setZoomLinkStr(event.target.value); }
+  const handleStartTimeStr = (event) => { setStartTimeStr(event.target.value); }
+  const handleTKTopicStr = (event) => { setTKTopicStr(event.target.value); }
   const handleStandUpStrChange = (event) => { setStandUpStr(event.target.value); }
   const handleModuleReviewAnnouncementStrChange = (event) => { setModuleReviewAnnouncementStr(event.target.value); }
   const handleModuleReviewStrChange = (event) => { setModuleReviewStr(event.target.value); }
-  const handleTKPrepChange = (event) => { setTKPrepStr(event.target.value); replaceStrVariable(moduleAssignmentStr, moduleReviewAnnouncementStr) }
+  const handleTKPrepChange = (event) => { setTKPrepStr(event.target.value); }
 
+  // const handleReplaceTextButton = (event) => {
+  //   event.preventDefault();
+  //   replaceKeyword(keyword, string)
+  // }
+
+  const replaceKeyword = () => {
+    let modifiedStr = standUpStr.replace("ZOOM_LINK", zoomLinkStr);
+    setStandUpStr(modifiedStr);
+  }
 
   // String.prototype.InsertAt=function(StrToInsert,Position){
   //   return this.slice(0,Position) + StrToInsert + this.slice(Position)
   // }
 
-  const replaceStrVariable = (str, formStr) => {
-    let replaceIndex = formStr.search(str) + str.length;
-    moduleReviewAnnouncementStr.InsertAt(str, replaceIndex)
-    setModuleReviewAnnouncementStr()
-  }
+  // const replaceStrVariable = (str, formStr) => {
+  //   let replaceIndex = formStr.search(str) + str.length;
+  //   moduleReviewAnnouncementStr.InsertAt(str, replaceIndex)
+  //   setModuleReviewAnnouncementStr(moduleReviewAnnouncementStr)
+  // }
+
 
   return (
     <Wrapper>
       <ToolTitle titleData={titleData}/>
       <CTAForm>
         <InputForm formName="Starting Time"
-          type="text"
-          formClassName="randomizer-time-form"
-          inputText={moduleAssignmentStr}
-          handleInputText={handleModuleAssignmentStr}
+          type="time"
+          formClassName="cta-starting-time-input"
+          inputText={startTimeStr}
+          handleInputText={handleStartTimeStr}
           placeHolderText={"HH:MM"}
-          maxLength="8"
-          />
+          maxLength="8"/>
+
+        <InputForm formName="Zoom Link"
+          type="text"
+          formClassName="cta-zoom-link-input"
+          inputText={zoomLinkStr}
+          handleInputText={handleZoomLinkStr}
+          placeHolderText={"Paste Zoom Link Here"}
+          maxLength="256"/>
+          
+        <InputForm formName="TK Topic"
+          type="text"
+          formClassName="cta-tk-topic-input"
+          inputText={TKTopicStr}
+          handleInputText={handleTKTopicStr}
+          placeHolderText={"Paste TK Topic Here"}
+          maxLength="256"/>
+
         <TextAreaForm formName="Stand Up"
-          formClassName="randomizer-names-form"
+          formClassName="stand-up-input"
           inputText={standUpStr}
           handleInputText={handleStandUpStrChange}
-          placeHolderText={""}
-          width={"100%"}
+          width={"45%"}
           resize={"none"}/>
+
         <TextAreaForm formName="Module Review Announcement"
-          formClassName="randomizer-names-form"
+          formClassName="module-review-accouncement-input"
           inputText={moduleReviewAnnouncementStr}
           handleInputText={handleModuleReviewAnnouncementStrChange}
-          placeHolderText={"Seperate names by a newline"}
-          width={"100%"}
+          width={"45%"}
           resize={"none"}/>
+
         <TextAreaForm formName="Module Review"
-          formClassName="randomizer-names-form"
+          formClassName="module-review-input"
           inputText={moduleReviewStr}
           handleInputText={handleModuleReviewStrChange}
-          placeHolderText={"Seperate names by a newline"}
-          width={"100%"}
+          width={"45%"}
           resize={"none"}/>
+
         <TextAreaForm formName="TK Review"
-          formClassName="randomizer-names-form"
+          formClassName="tk-review-input"
           inputText={tkPrepStr}
           handleInputText={handleTKPrepChange}
-          placeHolderText={"Seperate names by a newline"}
-          width={"100%"}
+          width={"45%"}
           resize={"none"}/>
+        {/* <Button onClickAction={handleTest}
+          title={"Test"}/> */}
       </CTAForm>
     </Wrapper>
   );
