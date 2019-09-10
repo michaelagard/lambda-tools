034542421571afb231398bec23bb3158358a963d
Michael Agard <mdagard@gmail.com>
2019-09-10 14:32:46 -0400

    add customhook/remove form handling from component

--- lambda-tools/src/components/StudentCTA/StudentCTA.js
+++ lambda-tools/src/components/StudentCTA/StudentCTA.js
@@ -1,47 +1,27 @@
 /*eslint no-extend-native: ["error", { "exceptions": ["Object"] }]*/
 
-import React, { useState } from 'react';
+import React from 'react';
 import ToolTitle from '../Subcomponents/ToolTitle/ToolTitle';
 import InputForm from '../Subcomponents/Forms/InputForm';
 import TextAreaForm from '../Subcomponents/Forms/TextAreaForm';
 import { Wrapper, CTAForm } from './StudentCTA.styled';
-import { standUpStrData, moduleReviewAnnouncementStrData, moduleReviewStrData, tkPrepStrData } from './Data/StudentCTAData';
-import Button from '../Subcomponents/Button/Button';
-
+import {useCTAForm} from './CustomHook';
 const titleData = {title: "Student CTAs", titleDescription: "For the truely lazy TL who just wants to prompt students to join their Zoom."}
 
 function StudentCTA() {
-
-  const [zoomLinkStr, setZoomLinkStr] = useState("");
-  const [startTimeStr, setStartTimeStr] = useState("");
-  const [TKTopicStr, setTKTopicStr] = useState("");
-
-  const [standUpStr, setStandUpStr ] = useState(standUpStrData);
-  const [moduleReviewAnnouncementStr, setModuleReviewAnnouncementStr] = useState(moduleReviewAnnouncementStrData);
-  const [moduleReviewStr, setModuleReviewStr] = useState(moduleReviewStrData);
-  const [tkPrepStr, setTKPrepStr] = useState(tkPrepStrData);
-
-  const handleZoomLinkStr = (event) => { setZoomLinkStr(event.target.value); }
-  const handleStartTimeStr = (event) => { setStartTimeStr(event.target.value); }
-  const handleTKTopicStr = (event) => { setTKTopicStr(event.target.value); }
-  const handleStandUpStrChange = (event) => { setStandUpStr(event.target.value); }
-  const handleModuleReviewAnnouncementStrChange = (event) => { setModuleReviewAnnouncementStr(event.target.value); }
-  const handleModuleReviewStrChange = (event) => { setModuleReviewStr(event.target.value); }
-  const handleTKPrepChange = (event) => { setTKPrepStr(event.target.value); }
+  const {inputs, handleInputChange, handleSubmit} = useCTAForm();
 
   // const handleReplaceTextButton = (event) => {
   //   event.preventDefault();
-  //   replaceKeyword(keyword, string)
+  //   replaceKeyword("ZOOM_LINK", zoomLinkStr)
   // }
 
-  const replaceKeyword = () => {
-    let modifiedStr = standUpStr.replace("ZOOM_LINK", zoomLinkStr);
-    setStandUpStr(modifiedStr);
-  }
-
-  // String.prototype.InsertAt=function(StrToInsert,Position){
-  //   return this.slice(0,Position) + StrToInsert + this.slice(Position)
+  // const replaceKeyword = (keyword, string) => {
+  //   let modifiedStr = standUpStr.replace(keyword, string);
+  //   setStandUpStr(modifiedStr);
   // }
+
+
 
   // const replaceStrVariable = (str, formStr) => {
   //   let replaceIndex = formStr.search(str) + str.length;
@@ -53,60 +33,65 @@ function StudentCTA() {
   return (
     <Wrapper>
       <ToolTitle titleData={titleData}/>
-      <CTAForm>
+      <CTAForm onSubmit={handleSubmit}>
         <InputForm formName="Starting Time"
+          name="startTime"
           type="time"
           formClassName="cta-starting-time-input"
-          inputText={startTimeStr}
-          handleInputText={handleStartTimeStr}
-          placeHolderText={"HH:MM"}
+          value={inputs.startTimeStr}
+          onChange={handleInputChange}
+          placeholder={"HH:MM"}
           maxLength="8"/>
 
         <InputForm formName="Zoom Link"
+          name="zoomLink"
           type="text"
           formClassName="cta-zoom-link-input"
-          inputText={zoomLinkStr}
-          handleInputText={handleZoomLinkStr}
-          placeHolderText={"Paste Zoom Link Here"}
+          value={inputs.zoomLinkStr}
+          onChange={handleInputChange}
+          placeholder={"Paste Zoom Link Here"}
           maxLength="256"/>
           
         <InputForm formName="TK Topic"
+          name="TKTopic"
           type="text"
           formClassName="cta-tk-topic-input"
-          inputText={TKTopicStr}
-          handleInputText={handleTKTopicStr}
-          placeHolderText={"Paste TK Topic Here"}
+          value={inputs.TKTopicStr}
+          onChange={handleInputChange}
+          placeholder={"Paste TK Topic Here"}
           maxLength="256"/>
 
         <TextAreaForm formName="Stand Up"
+          name="standUpInput"
           formClassName="stand-up-input"
-          inputText={standUpStr}
-          handleInputText={handleStandUpStrChange}
+          value={inputs.standUpStr}
+          onChange={handleInputChange}
           width={"45%"}
           resize={"none"}/>
 
         <TextAreaForm formName="Module Review Announcement"
+          name="moduleReviewAnnouncement"
           formClassName="module-review-accouncement-input"
-          inputText={moduleReviewAnnouncementStr}
-          handleInputText={handleModuleReviewAnnouncementStrChange}
+          value={inputs.moduleReviewAnnouncementStr}
+          onChange={handleInputChange}
           width={"45%"}
           resize={"none"}/>
 
         <TextAreaForm formName="Module Review"
+          name="moduleReview"
           formClassName="module-review-input"
-          inputText={moduleReviewStr}
-          handleInputText={handleModuleReviewStrChange}
+          value={inputs.moduleReviewStr}
+          onChange={handleInputChange}
           width={"45%"}
           resize={"none"}/>
 
         <TextAreaForm formName="TK Review"
+          name="TKReview"
           formClassName="tk-review-input"
-          inputText={tkPrepStr}
-          handleInputText={handleTKPrepChange}
+          inputText={inputs.tkPrepStr}
+          onChange={handleInputChange}
           width={"45%"}
           resize={"none"}/>
-        {/* <Button onClickAction={handleTest}
-          title={"Test"}/> */}
       </CTAForm>
     </Wrapper>
   );
